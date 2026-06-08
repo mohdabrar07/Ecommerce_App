f27443f166e0f158512f12a61ce79c13e95552a3
Mohammad Abrar Ahammad <153979580+mohdabrar07@users.noreply.github.com>
2026-06-08 11:38:31 +0530

    feat:initial project setupwith clean architecture

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      home:Scaffold(
+        body:Center(
+          child: Text("Ecommerce App"),
+        ),
+      ),
+    );
+  }
+}
